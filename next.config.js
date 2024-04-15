b25affd6266e5c36b526faaaba13fcc0da327b9c
luismarioamor <luismarioamorsanchez@gmail.com>
2024-04-14 23:16:46 -0500

    environment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+const prod = process.env.NODE_ENV === "production";
+
+const url = prod ? "https://host-shell.vercel.app" : "http://localhost:3000";
 const remotes = (isServer) => {
   const location = isServer ? "ssr" : "chunks";
   return {
-    hostShell: `host-shell@http://localhost:3000/_next/static/${location}/remoteEntry.js`,
+    hostShell: `host-shell@${url}/_next/static/${location}/remoteEntry.js`,
   };
 };
 const nextConfig = {
